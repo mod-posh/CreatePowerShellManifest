c07abcbacfdfbeb74974b5f72951b344181682e9
Jeff Patton <jeffrey@patton-tech.com>
2024-07-18 10:52:34 -0500

    ManifestPath contains the filename plus extension

--- createpowershellmanifest.ps1
+++ createpowershellmanifest.ps1
@@ -76,7 +76,7 @@ try
     write-host "Start copy"
     Get-Item -Path "$($ModuleRoot)\$($ModuleName).psd1"
     Copy-Item "$($ModuleRoot)\$($ModuleName).psd1" -Destination $outputPath -ErrorAction Stop
-    Get-Item -Path "$($ManifestPath).psd1"
+    Get-Item -Path $ManifestPath
     (Get-ChildItem -Path $outputPath -Recurse).FullName
     Write-Host "Copied module manifest to destination"
     Write-Host "::endgroup::"
